dbf2fab8f84faccd8f5e5000251a28377c37d416
PeterKrneta <peterkrneta55@gmail.com>
2022-05-04 15:24:33 +0200

    Dodal vislice

---
+++ model.py
@@ -0,0 +1,15 @@
+STEVILO_DOVOLJENIH_NAPAK = 10
+PRAVILNA_CRKA = '+'
+PONOVLJENA_CRKA = 'o'
+NAPACNA_CRKA = '-'
+ZMAGA = 'W'
+PORAZ = 'X'
+
+class Igra():
+    pass
+class Igra:
+
+    def __init__(self, geslo, crke):
+        self.geslo = geslo
+        self.crke = crke
+
